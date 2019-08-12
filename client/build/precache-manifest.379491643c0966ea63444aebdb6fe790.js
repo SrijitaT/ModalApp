e17ea60720e0eb36304dc88848ff61649e50b0ce
Srijita Thakur <srijita.thakur@gmail.com>
2019-08-13 02:31:49 +0530

    added build folder

---
+++ client/build/precache-manifest.379491643c0966ea63444aebdb6fe790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7ad9ad5dd062eb14bafc72a842dd52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa7241fe9817bb59d97e",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0e15265703da5934a439",
+    "url": "/static/css/main.825d2be2.chunk.css"
+  },
+  {
+    "revision": "fa7241fe9817bb59d97e",
+    "url": "/static/js/2.bee24376.chunk.js"
+  },
+  {
+    "revision": "0e15265703da5934a439",
+    "url": "/static/js/main.fe41d0f6.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
